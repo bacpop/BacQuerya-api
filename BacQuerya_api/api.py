0da63814529c1f40c5e277832887116daf43c209
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2021-03-26 11:29:05 +0000

    Get locations of necessary files from env

--- BacQuerya_api/api.py
+++ BacQuerya_api/api.py
@@ -10,9 +10,17 @@ from urllib.parse import unquote
 
 from paper_search import search_pubmed
 
-sys.path.insert(1, '..')
+# data locations
+gene_kv_file = '/home/bacquerya-usr/' + os.getenv('GENE_DICT_FILE')
+gene_index = '/home/bacquerya-usr/' + os.getenv('GENE_INDEX')
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.update(
+    TESTING=True,
+    SCHEDULER_API_ENABLED=True,
+    SECRET_KEY=os.environ.get('FLASK_SECRET_KEY')
+)
+
 CORS(app, expose_headers='Authorization')
 
 @app.route('/sequence', methods=['POST'])
@@ -25,12 +33,12 @@ def postSeqResult():
         query_sequence = sequence_dict['searchTerm']
         # search for uploaded sequence in COBS index
         sys.stderr.write("\nSearching COBS index\n")
-        index_name = "index_genes/31_index.cobs_compact"
+        index_name = gene_index
         index = cobs.Search(index_name)
         result = index.search(query_sequence, threshold = 0.8)
         # load metadata for identified sequences
         sys.stderr.write("\nLoading gene metadata\n")
-        with open("extracted_genes/panarooPairs.json") as f:
+        with open(gene_kv_file) as f:
             geneJSON = f.read()
         genePairs = json.loads(geneJSON)
         query_length = len(query_sequence)
@@ -68,6 +76,5 @@ def paperSearch():
         return jsonify({"result": searchResult})
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(24)
     app.run(debug=False,use_reloader=False)
 
