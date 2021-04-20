4b69fb782a77c3c39d186302e0c23812729153b4
Danderson123 <danielanderson1@hotmail.com>
2021-04-20 15:57:59 +0100

    Add output_dir to bulk_download function call

--- BacQuerya_api/api.py
+++ BacQuerya_api/api.py
@@ -104,7 +104,7 @@ def bulkDownload():
             os.mkdir(raw_temp_dir)
         urlDict = request.json
         urlList = urlDict["sequenceURLs"]
-        tarFilePath = getDownloadLink(urlList, temp_dir, raw_temp_dir, n_cpu)
+        tarFilePath = getDownloadLink(urlList, output_dir, temp_dir, raw_temp_dir, n_cpu)
         s = Serializer(app.config['SECRET_KEY'], expires_in=60*60*24) # temporary URL live for 60 secs by 60 min by 24 hours
         token = s.dumps({'file_path': tarFilePath}).decode("utf-8")
         url_for('serve_file', token=token)
